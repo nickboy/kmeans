1e9c751182552594b0923c686cbd1c452e67eaf4
Nick Liu <nickboy0211@hotmail.com>
2013-12-09 17:06:58 -0600

    add find_the_most_distant_point and clustering functions

--- kmeans.rb
+++ kmeans.rb
@@ -21,18 +21,52 @@ def euclidean_distance(p1,p2)
 			sum_of_squares += (p1_coord - p2[index]) ** 2 
 		end
 	end
-	puts Math.sqrt( sum_of_squares )
+	return Math.sqrt( sum_of_squares )
 end
 
 def find_the_most_distant_point(centroids, points)
-	centroid = centroids[0]
-	points.
-
+	first_centroid = centroids[0]
+	puts first_centroid
+	max = 0
+	fartest_index = 0
+	points.each_with_index do |centroid,index|
+		distance = euclidean_distance(first_centroid, points[index])
+		puts "distance is #{distance}"
+		unless max > distance
+			max = distance
+			fartest_index=index
+		end
+	end
+	puts "max distance is #{max}, which is #{points[fartest_index]}"
+	return points[fartest_index]
 end
+
+#distribute every points to nearest centroids.
+def clustering(centroids, points)
+	points.each_with_index do |point, point_index|
+		min = 10000000000000000
+		nearest_index = 0
+		centroids.each_with_index do |centroid, centroid_index|
+			distance = euclidean_distance(point,centroid)
+			puts centroid.class
+			unless distance > min || point == centroid
+				min = distance
+				nearest_index = centroid_index
+				puts "#{point} belongs to centroid #{centroid_index}"
+			end
+		end
+	end
+end
+
+
 #import dataset then convert into array
 data = CSV.read(ARGV[0], :converters => :all)
 
 #random select first centroid
-centroids = data[rand(data.length)]
+centroids.push data[rand(data.length)]
 puts "First centroid: #{centroids}"
 
+#push second centroid to the centroid array
+centroids.push find_the_most_distant_point(centroids,data)
+puts centroids
+clustering(centroids, data)
